ec008d1adfe679864dfcd17ae0a4d9c0ffda98ac
YamSok <mika.elouazz@hotmail.fr>
2020-12-28 20:33:48 +0100

    update: debig

--- preprocessing2.py
+++ preprocessing2.py
@@ -7,7 +7,7 @@ def main():
     r = DataReader.PdData()
     years = range(1,3)
     train = dict(zip(years, 
-                    [r.massaged_data(x, hourly=False).dropna() for x in years]))
+                    [r.massaged_data(x, hourly=True).dropna() for x in years]))
     train_all = pd.concat(train.values(), axis=0).sort_index()
 
     timestamp = d.datetime.now().strftime("%d-%m-%Y(%H:%M:%S)")
