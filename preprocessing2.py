cb263540d73946fd5aa8cb22777197636d8fba8f
YamSok <mika.elouazz@hotmail.fr>
2020-12-28 19:51:32 +0100

    add: nouveau preprocessing

---
+++ preprocessing2.py
@@ -0,0 +1,17 @@
+import DataReader 
+import pandas as pd
+import datetime as d
+
+
+def main():
+    r = DataReader.PdData()
+    years = range(1,3)
+    train = dict(zip(years, 
+                    [r.massaged_data(x, hourly=False).dropna() for x in years]))
+    train_all = pd.concat(train.values(), axis=0).sort_index()
+
+    timestamp = d.datetime.now().strftime("%d-%m-%Y(%H:%M:%S)")
+    train_all.to_pickle(f"data/train_{timestamp}.p")
+
+
+main()
