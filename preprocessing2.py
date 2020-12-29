9183824d137b63e1aee2645da0cfc5c56ac19e54
YamSok <mika.elouazz@hotmail.fr>
2020-12-29 12:26:16 +0100

    debug

--- preprocessing2.py
+++ preprocessing2.py
@@ -6,18 +6,18 @@ import datetime as d
 def main():
     r = DataReader.PdData()
     years = range(1,3)
-    train = dict(zip(years, 
-                    [r.massaged_data(x, hourly=True).dropna() for x in years]))
-    
+    # train = dict(zip(years, 
+    #                 [r.massaged_data(x, hourly=True).dropna() for x in years]))
+    train = r.massaged_data(1, hourly=True)
     print("~"*25)
     print(train)
-    train_all = pd.concat(train.values(), axis=0).sort_index()
+    # train_all = pd.concat(train.values(), axis=0).sort_index()
     print("~"*25)
 
-    print(train_all.shape)
+    print(train.shape)
     
     timestamp = d.datetime.now().strftime("%d-%m-%Y(%H:%M:%S)")
-    train_all.to_pickle(f"data/train_{timestamp}.p")
+    train.to_pickle(f"data/train_{timestamp}.p")
 
 
 main()
